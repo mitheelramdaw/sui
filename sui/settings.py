d4b8991e7fbae857de02438551b639cef0a88ac2
Mitheel Ramdaw <106301219+mitheelramdaw@users.noreply.github.com>
2024-09-05 22:37:34 +0200

    Nav bar added - syling not loading - FIx static loading

--- sui/settings.py
+++ sui/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    'backend',
+    'backend',  # Your app
 ]
 
 MIDDLEWARE = [
@@ -55,7 +55,7 @@ ROOT_URLCONF = "sui.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [BASE_DIR / 'backend/templates'],  # Added your templates directory
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
@@ -116,7 +116,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
+
+# Add static files directory
+STATICFILES_DIRS = [
+    BASE_DIR / 'backend/static',  # This is where you will store static files like CSS/JS
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
